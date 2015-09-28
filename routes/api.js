bd2aeba78f53939063f48b96e84ed5d18b9b4f85
Reed Wilson <reedcwilson@gmail.com>
2015-09-27 23:34:12 -0600

    add predicate creation to controller

--- routes/api.js
+++ routes/api.js
@@ -5,10 +5,17 @@ var router = express.Router();
 
 var nameManager = require('../name_manager');
 
+var createPredicate = function(gender, startsWith) {
+  gender = gender.toLowerCase();
+  startsWith = startsWith.toLowerCase();
+  return (n) => (startsWith == undefined ? true : n.startsWith(startsWith) && 
+    ((gender == undefined || gender === 'both') ? true : (gender === 'male' ? n.isMale() : n.isFemale())))
+};
+
 /* POST the n most popular names. */
 router.post('/popular', function(req, res, next) {
   res.setHeader('Content-Type', 'application/json');
-  let names = nameManager.getMostPopular(req.body.range, req.body.number, n => n.startsWith(req.body.startsWith.toLowerCase()));
+  let names = nameManager.getMostPopular(req.body.range, req.body.number, createPredicate(req.body.gender, req.body.startsWith));
   res.send(names);
 });
 
