92195d59e080bf7313b53f450ad2fb82ec8c11e7
Reed Wilson <reedcwilson@gmail.com>
2015-09-30 10:10:12 -0600

    ensure that the body is JSON

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,9 @@ var router = express.Router();
 
 var nameManager = require('../name_manager');
 
+//
+// helpers
+//
 var createPredicate = function(gender, startsWith) {
   if (gender != undefined) {
     gender = gender.toLowerCase();
@@ -16,6 +19,9 @@ var createPredicate = function(gender, startsWith) {
     ((gender == undefined || gender === 'both') ? true : (gender === 'male' ? n.isMale() : n.isFemale())))
 };
 
+//
+// validation methods
+//
 var validateRange = function(range) {
   return range !== undefined && Array.isArray(range) && range.length == 2 && range[0] > 1879 && range[1] < 2015 && range[0] <= range[1];
 };
@@ -32,9 +38,23 @@ var validateStartsWith = function(startsWith) {
   return /^[a-zA-Z]+$/.test(startsWith);
 };
 
+//
+// endpoints
+//
+
 /* POST the n most popular names. */
 router.post('/popular', function(req, res, next) {
   var messages = [];
+  var contentType = req.get('Content-Type');
+
+  // validate that the body is JSON
+  if (!contentType || contentType.toLowerCase() !== 'application/json') {
+    res.status(400);
+    res.send({ "error": "Request must have a header of 'Content-Type: application/json'" });
+    return;
+  }
+
+  // validate the parameters
   if (!validateRange(req.body.range)) {
       messages.push("'range' is required and must be an array of two. The first value must be greater than or equal to 1880 and less than or equal to the second value. The second value must be less than 2015.");
   }
@@ -47,10 +67,13 @@ router.post('/popular', function(req, res, next) {
   if (!validateStartsWith(req.body.startsWith)) {
       messages.push("'startsWith' must be a string of alpha characters");
   }
+
+  // if there are errors, send a BAD_REQUEST response
   if (messages.length > 0) {
     res.status(400);
     res.send({ "errors": messages });
   }
+  // otherwise perform the query
   else {
     res.setHeader('Content-Type', 'application/json');
     let names = nameManager.getMostPopular(req.body.range, req.body.number, createPredicate(req.body.gender, req.body.startsWith));
