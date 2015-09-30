9542e9b6f36e07b42816923bc1d43d0e81d694b9
Reed Wilson <reedcwilson@gmail.com>
2015-09-30 16:17:13 -0600

    then after if

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,17 +4,19 @@ echo "Host heroku.com" >> ~/.ssh/config
 echo "   StrictHostKeyChecking no" >> ~/.ssh/config
 echo "   CheckHostIP no" >> ~/.ssh/config; 
 echo "   UserKnownHostsFile=/dev/null" >> ~/.ssh/config;
-if [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master" ]] then
-  gem install heroku
-  heroku keys:clear
-  echo yes | heroku keys:add
-  git checkout .
-  git remote add heroku https://git.heroku.com/name-prospector.git 
-  git subtree push --prefix=server heroku master
-  heroku keys:clear
+if [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master" ]] 
+  then
+    gem install heroku
+    heroku keys:clear
+    echo yes | heroku keys:add
+    git checkout .
+    git remote add heroku https://git.heroku.com/name-prospector.git 
+    git subtree push --prefix=server heroku master
+    heroku keys:clear
 fi
-if [[ $TRAVIS_PULL_REQUEST == "false" ]] then
-  echo $TRAVIS_BRANCH
+if [[ $TRAVIS_PULL_REQUEST == "false" ]] 
+  then
+    echo $TRAVIS_BRANCH
 fi
 echo
 echo "...done."
